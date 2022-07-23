b5d26926ccfc47dbe685e012c011e2018f604f76
Caleb McGraw <calebem1998@gmail.com>
2022-07-23 14:56:22 -0400

    Basis Creation

---
+++ ScheduleMaker2.0/Employee.cs
@@ -0,0 +1,55 @@
+﻿
+//construct employee data as an interface
+public class Employee {
+	private String fullName = "none";
+	private String phoneNumber = "-1";
+	private int numOfShifts = -1;
+	private bool[] availability = new bool[26];
+	private bool lead = false;
+	private bool server = false;
+	private bool bistro = false;
+
+	public string FullName { get => fullName; set => fullName = value; }
+	public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+	public int NumOfShifts { get => numOfShifts; set => numOfShifts = value; }
+	public bool[] Availability { get => availability; set => availability = value; }
+	public bool Lead { get => lead; set => lead = value; }
+	public bool Server { get => server; set => server = value; }
+	public bool Bistro { get => bistro; set => bistro = value; }
+	public bool getAvailableAt(int day) {
+		return Availability[day];
+	}
+	public void setAvailableAt(int day, bool canWork) {
+		Availability[day] = canWork;
+	}
+
+}
+
+//abstracted class for people who are leads
+public class Lead : Employee {
+	
+	Lead(bool[] availability, bool bistro) {
+		Lead = true;
+		Server = true;
+		Availability = availability;
+		Bistro = bistro;
+	}
+}
+
+//abstracted class for people who are non-lead servers 
+public class Server : Employee {
+	Server(bool[] availability, bool bistro) {
+		Server = true;
+		Availability = availability;
+		Bistro = bistro;
+	}
+}
+
+//abstracted class for people who are non-lead non-servers
+public class Assistant : Employee {
+	Assistant(bool[] availability, bool bistro) {
+		Availability = availability;
+		Availability = availability;
+		Bistro = bistro;
+	}
+}
--- ScheduleMaker2.0/Program.cs
+++ ScheduleMaker2.0/Program.cs
@@ -1,5 +1,34 @@
 ﻿
 
+//array values of availabilities
+const int fridayAM1 = 0;
+const int fridayPM1 = 1;
+const int saturdayAM1 = 2;
+const int saturdayPM1 = 3;
+const int sunday1 = 4;
+const int mondayAM1 = 5;
+const int mondayPM1 = 6;
+const int tuesdayAM1 = 7;
+const int tuesdayPM1 = 8;
+const int wednesdayAM1 = 9;
+const int wednesdayPM1 = 10;
+const int thursdayAM1 = 11;
+const int thursdayPM1 = 12;
+
+const int fridayAM2 = 13;
+const int fridayPM2 = 14;
+const int saturdayAM2 = 15;
+const int saturdayPM2 = 16;
+const int sunday2 = 17;
+const int mondayAM2 = 18;
+const int mondayPM2 = 19;
+const int tuesdayAM2 = 20;
+const int tuesdayPM2 = 21;
+const int wednesdayAM2 = 22;
+const int wednesdayPM2 = 23;
+const int thursdayAM2 = 24;
+const int thursdayPM2 = 25;
+
 namespace ScheduleMaker
 {
 	class Program
